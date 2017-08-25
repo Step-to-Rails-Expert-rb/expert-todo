c88ed1e80c23b9e89239a9873b99bf42babb72e7
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-08-25 16:11:45 +0900

    Create tasks table
    - Change the class, `CreateTasks`
    - Execute `bin/rails db:migrate`

--- db/migrate/20170825064634_create_tasks.rb
+++ db/migrate/20170825064634_create_tasks.rb
@@ -1,7 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
-
+      t.references :user, null: false, foreign_key: true, index: true
+      t.string :task_name, null: false
+      t.text :description
+      t.datetime :due_date
       t.timestamps
     end
   end
