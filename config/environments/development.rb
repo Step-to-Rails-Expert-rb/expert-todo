774e182f4889889669eceec1f9a3024fb940db62
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-08-25 17:21:14 +0900

    Add bullet gem configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,10 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  
+  # bullet configurations
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
 end
