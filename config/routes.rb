15ca23c645a0216c63823e811a924bdf9a8e29e9
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-08-27 20:33:40 +0900

    :cop: rubocop --auto-correct --only Layout/IndentHash

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "welcome#index"
   devise_for :users, controllers: {
-      sessions: "users/sessions",
+    sessions: "users/sessions",
   }
   
   resources :tasks, except: [:show]
