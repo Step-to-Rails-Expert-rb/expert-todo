d4fa92f197da9e3ce2835a17c58088a83b94b3a8
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-08-27 20:31:29 +0900

    Create task request spec
    * Change rails_helper adding the devise test helper configuration
    * Create a request spec of the task index page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  
   config.before(:suite) do
     DatabaseRewinder.clean_all
   end
---
+++ spec/requests/tasks_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "Tasks", type: :request do
+  
+  let(:task) {create(:task)}
+  
+  
+  describe "GET /tasks" do
+    before do
+      sign_in task.user
+    end
+    
+    it "responses with 200" do
+      get "/tasks"
+      expect(response).to have_http_status(200)
+    end
+  end
+
+end
