425f62cbd3954e7f44af4116922b277dd86dd8c3
ttw <ttwo32@hotmail.com>
2017-09-10 19:11:28 +0900

    Add sendgrid for sending remember  password mail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,19 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { host: 'two-sann-expert-todo.herokuapp.com', port: 3000 }
+
+  # Setup the mailer config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'two-sann-expert-todo.herokuapp.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+
+
 end
