fffa33515dc1c087b6cf3415803b7f53d6e60022
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-09-19 00:17:38 +0900

    Change locale en to ja and use translation in view files

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def login_or_logout_link(current_user)
     if current_user
-      link_to "Logout", destroy_user_session_path, method: "DELETE"
+      link_to t("header.logout"), destroy_user_session_path, method: "DELETE"
     else
-      link_to "Login", new_user_session_path
+      link_to t("header.login"), new_user_session_path
     end
   end
 end
