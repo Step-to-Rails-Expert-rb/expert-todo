f47871645965343dafe8c1053c57f1ed850b9e95
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-09-19 00:14:26 +0900

    Change timezone and fix the spec that fails beacause of the timezone configration

--- spec/decorators/task_decorator_spec.rb
+++ spec/decorators/task_decorator_spec.rb
@@ -16,12 +16,6 @@ describe "#due_date_without_time" do
     end
 
     it "returns due_date without time and timezone" do
-      pending "This example fails around 01:00 am because of timezone difference"
-      raise
-      # DateTime.now
-      #=> Tue, 12 Sep 2017 01:13:28 +0900
-      # task
-      # due_date: Mon, 11 Sep 2017 16:13:28 UTC +00:00,
       task = build_stubbed(:task, :due_date)
       decorated_task = ActiveDecorator::Decorator.instance.decorate(task)
       expect(decorated_task.due_date_without_time).to eq(DateTime.now.strftime("%Y-%m-%d"))
