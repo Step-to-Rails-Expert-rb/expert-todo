f7fef1cece589cf6b6dc4fe4d5220ae7ce6a9148
Yusuke Ebihara <dev.biibiebi@gmail.com>
2017-09-20 00:27:04 +0900

    :cop: rubocop --auto-correct --only Style/StringLiterals

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,2 +1,2 @@
 Rails.application.config.i18n.default_locale = :ja
-Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+Rails.application.config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
