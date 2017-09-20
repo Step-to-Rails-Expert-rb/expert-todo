96f2c994cf6464efd75d79bd99b8937c88ae19c6
ttw <ttwo32@hotmail.com>
2017-09-20 21:23:14 +0900

    Add project edit page.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
   end
 
   def create
@@ -12,7 +13,7 @@ class ProjectsController < ApplicationController
     project_member.user_id = current_user.id
     project_member.role = 1
     if project.save
-      redirect_to :action => "index" , notice: 'success form making project.'
+      redirect_to :action => "index" , notice: 'success for making project.'
     else
       render 'new'
     end
@@ -22,8 +23,25 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_update_params)
+      redirect_to project_path(@project) , notice: 'success for updating project.'
+    else
+      render 'edit'
+    end
+  end
+
   private
   def project_create_params
+    params.require(:project).permit(:name)
+  end
+
+  def project_update_params
     params.require(:project).permit(:name)
   end
 end
