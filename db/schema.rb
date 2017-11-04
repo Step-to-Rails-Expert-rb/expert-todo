4a49b1c0c9e9ce61c8959a4c54b8b09a4e4e5f2e
284km <k.furuhashi10@gmail.com>
2017-11-04 16:52:47 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104073148) do
+ActiveRecord::Schema.define(version: 20171104074124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20171104073148) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.datetime "deadlien"
+    t.date "deadlien"
     t.boolean "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
