af46c88dcb829eb2d6496345806d404ff83f2f68
284km <k.furuhashi10@gmail.com>
2018-01-29 16:44:47 +0900

    rails g migration AddUserToTodos user:references && rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104074124) do
+ActiveRecord::Schema.define(version: 20180129074355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20171104074124) do
     t.boolean "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema.define(version: 20171104074124) do
   end
 
   add_foreign_key "tasks", "users"
+  add_foreign_key "todos", "users"
 end
