f4af06bf560204d5989f14eaebecebd02ade3167
284km <k.furuhashi10@gmail.com>
2018-01-29 17:59:32 +0900

    Annodate models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Task < ApplicationRecord
   belongs_to :user
 end
