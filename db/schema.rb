f10623a6508edf2f53124c13edfd846e3a4f6d78
syhashi <50n50n.xarc@gmail.com>
2018-03-19 01:58:20 +0900

    bundle exec rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318153715) do
+ActiveRecord::Schema.define(version: 20180318164816) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
